8e7df305d71b2e90969087a91d92f96b5cb90b71
jc <jancarluzdeleon226@gmail.com>
2024-10-04 14:39:35 +0800

    Update TextFileBufferedReader.java

--- TextFileBufferedReader.java
+++ TextFileBufferedReader.java
@@ -5,7 +5,7 @@ public class TextFileBufferedReader {
 	public static void main(String[] args) throws Exception
 	{
 	
-		File file = new File("C:\\Users\\28\\Downloads\\dadada\\jancarluzdeleon.txt");
+		File file = new File("jancarluzdeleon.txt");
 		
 		BufferedReader br = new BufferedReader(new FileReader(file));
 		String st;
