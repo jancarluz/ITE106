fc46f101fc54847d6ee5a000457a2577f481a5b5
jc <jancarluzdeleon226@gmail.com>
2024-10-04 14:41:52 +0800

    Update TextFileManipulatorFileReader.java

--- TextFileManipulatorFileReader.java
+++ TextFileManipulatorFileReader.java
@@ -5,7 +5,7 @@ public class TextFileManipulatorFileReader
 	public static void main(String[] ards) throws Exception
 	{
 
-	File file = new File("C:\\Users\\CLAB01-PCE45\\Documents\\fsds\\jancarluzdeleon.txt");
+	File file = new File("jancarluzdeleon.txt");
 	Scanner sc = new Scanner(file);
 	
 	while (sc.hasNextLine())
