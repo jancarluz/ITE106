9fbb2f4073f619127a94456fe93ae91dfe40adb5
jc <jancarluzdeleon226@gmail.com>
2024-10-14 13:32:51 +0800

    Create Lamp3.java

---
+++ Lamp Program/Lamp3.java
@@ -0,0 +1,13 @@
+public class Lamp3
+{
+	public String name;
+	public boolean isOn;
+	
+	void turnOn() {
+		System.out.println("Light on? " + isOn);
+	}
+	
+	void turnOff() {
+		System.out.println("Light on? " + isOn);
+	}
+}
