d7221d8d6a7d43a9a567ef89ffb7494e20ab6da4
jc <jancarluzdeleon226@gmail.com>
2024-11-06 20:37:32 +0800

    Create MultipleInheritance.java

---
+++ Activity Types of inheritance/MultipleInheritance/MultipleInheritance.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+interface one {
+    public void print_geek();
+}
+
+interface two {
+    public void print_for();
+}
+
+interface three extends one, two {
+    public void print_geek();
+}
+class Child implements three {
+    @Override public void print_geek()
+    {
+        System.out.println("Geeks");
+    }
+
+    public void print_for() { System.out.println("for"); }
+}
