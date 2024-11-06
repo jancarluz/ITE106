b71b8c0320ab3d1ec6d5f0614af0e42cde39053e
jc <jancarluzdeleon226@gmail.com>
2024-11-06 20:40:57 +0800

    Create Main.java

---
+++ Activity Types of inheritance/MultipleLevelInheritance/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) 
+   {        
+        three g = new three();     
+        g.print_geek();
+        g.print_for();
+        g.print_lastgeek();
+    }
+}
